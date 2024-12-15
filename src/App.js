1fa6aa9c2e76b75bdf7dafef08a9243c4a2ab590
JacksonSmith43 <jessica444@gmx.at>
2024-12-15 15:01:05 +0100

    Finished Local Storage I.

--- src/App.js
+++ src/App.js
@@ -3,40 +3,36 @@ import './App.css';
 
 function App() {
 
-
   /* Local Storage I stuff. */
-  // 1. Check if something has been inputed.
-  // 2. useState to safe the state of the word.
-  // 
-  const [isWordInputed, setIsWordInputed] = useState(false);
-  const [isSavedWord, setIsSavedWord] = useState();
-  const [isPageReloaded, setIsPageReloaded] = useState();
-
+  /* TODO:  
+      1. Input word.
+      2. Save word: setItem.
+      3. Reload page: Check whether it has been reloaded. 
+      4. Add word from localStorage to the input. 
+    */
   const userInput = useRef();
 
-  useEffect(() => {
-    setIsSavedWord(localStorage.setItem("userInput", JSON.stringify(userInput.current.value)));
-  }, [userInput.current]);
-
-
-  function checksReload() {
-    if (performance.getEntriesByType("navigation")[0]?.type === "reload") { // Checks whether the page has been reloaded. /// Needed help here.  
-      setIsPageReloaded(true);
+  useEffect(() => { /// Needed help here. 
+    if (performance.getEntriesByType("navigation")[0]?.type === "reload") { // Checks whether the page has been reloaded.  
       console.log("Page reload.");
-
-    } else {
-      setIsPageReloaded(false);
     }
 
-  }
+    const getsWord = localStorage.getItem('userInput');
+    if (getsWord && userInput.current) { // If getsWord is true then userInput.current will be rendered. 
+      userInput.current.value = JSON.parse(getsWord);
+    }
+  }, []); // It is empty, because it is only supposed to load, when the page is being loaded. 
 
+
+  function handleSave() {
+    const savesWord = localStorage.setItem("userInput", JSON.stringify(userInput.current.value));
+  }
 
   return (
     <div className='input-local-storage-container'>
-      <input ref={userInput} onChange={checksReload} value={`${isWordInputed ? JSON.parse(localStorage.getItem('userInput')) : ""}`} type='text' placeholder='Add a word.'></input>
+      <input ref={userInput} onChange={handleSave} type='text' placeholder='Add a word.'></input>
     </div>
   );
-
 }
 
 export default App;
