6a0e892658b7ae8481ed824755ca5815983efb36
JacksonSmith43 <jessica444@gmx.at>
2024-12-16 18:53:20 +0100

    Split it into Components.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import ProgressBar from './components/ProgressBar';
+import LocalStorage from './components/LocalStorage';
 
 function App() {
 
-  /* Local Storage I stuff. */
-  /* TODO:  
-      1. Input word.
-      2. Save word: setItem.
-      3. Reload page: Check whether it has been reloaded. 
-      4. Add word from localStorage to the input. 
-    */
-  const userInput = useRef();
-
-  useEffect(() => { /// Needed help here. 
-    if (performance.getEntriesByType("navigation")[0]?.type === "reload") { // Checks whether the page has been reloaded.  
-      console.log("Page reload.");
-    }
-
-    const getsWord = localStorage.getItem('userInput');
-    if (getsWord && userInput.current) { // If getsWord is true then userInput.current will be rendered. 
-      userInput.current.value = JSON.parse(getsWord);
-    }
-  }, []); // It is empty, because it is only supposed to load, when the page is being loaded. 
-
-
-  function handleSave() {
-    const savesWord = localStorage.setItem("userInput", JSON.stringify(userInput.current.value));
-  }
-
   return (
-    <div className='input-local-storage-container'>
-      <input ref={userInput} onChange={handleSave} type='text' placeholder='Add a word.'></input>
-    </div>
+    <>
+      <LocalStorage />
+      <ProgressBar />
+    </>
+
   );
 }
 
---
+++ src/components/ProgressBar.js
@@ -0,0 +1,13 @@
+//import styled from 'styled-components'
+
+const ProgressBar = ({ percent = 12 }) => {
+
+    return (
+        <div data-testid="wrapper">
+            <div data-testid="progress" />
+            <p>Hola.</p>
+        </div>
+    );
+};
+
+export default ProgressBar;
