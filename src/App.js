3d195cfd06f932efc95a3dcda5c47de082d5cbba
JacksonSmith43 <jessica444@gmx.at>
2024-12-23 15:19:35 +0100

    A functioning progress bar.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import './App.css';
 import ProgressBar from './components/ProgressBar';
 import LocalStorage from './components/LocalStorage';
-import styled from 'styled-components';
 
 function App() {
-
-  // TODO: This makes it so that the Component is not as it should be. 
-  const ProgressBar = styled.button` 
-    background-color: green;
-    width: 100%;
-    height: 20px;
-    border-radius: 5px;
-    margin-top: 4rem;
-  `
 
   return (
     <>
       <LocalStorage />
       <ProgressBar />
     </>
-
   );
 
 
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,13 +1,32 @@
 import styled from 'styled-components'
+// REMINDER: Needed a bit of help here. For example: const Progress = styled.button vs. HTML and CSS Code and where what is allowed to be 
 
-const ProgressBar = ({ percent = 12 }) => {
+const Progress = styled.button` 
+    width: ${(props) => props.percent || 0}%;
+    height: 2rem;
+    background: green;
+    border-radius: 0.5rem;
+    border-color: green;
+    transition: width 0.3s ease-in-out;
+`
+
+// styled.div created a new React component that is a styled div Element. 
+const ProgressBarContainer = styled.div`    
+    width: 100%;
+    height: 2rem;
+    background-color: rgb(196, 186, 186);
+    border-radius: 0.5rem;
+    margin-top: 1rem;
+`
+
+const ProgressBar = ({ percent }) => {
     return (
-        <div data-testid="progress-bar-container">
-            <h3>Progress Bar:</h3>
-            <div data-testid="progress" />
-            <button></button>
-            <button>2. Progress bar.</button>
-        </div>
+        <>
+            <h3 >Progress Bar:</h3>
+            <ProgressBarContainer>
+                <Progress percent={80} />
+            </ProgressBarContainer>
+        </>
     );
 };
 
